8754c16a38682469b208a94c25743689930833b2
cyber-end-d <marina_jeanette@hotmail.com>
2020-11-17 10:28:41 -0300

    merge

--- nuevo.py
+++ nuevo.py
@@ -1,56 +1,71 @@
+# hay ciertos conceptos del POO que no se utilizan y podrian mejorar el programa
+# como polimorfismo o delegacion (no se delega a clases inferiores el calcular precio)
+# ademas el codigo esta muy acoplado, para agregar la clase premium deberia agregar un if mas en precio
+# self.cl es poco expresivo en la clase Pasaje o TuristaEst podria ser TuristaEstudiante
+
 import datetime
 
 class Pasaje:
     def __init__(self, cliente):
         self.cliente = cliente
+        #self.precio = self.cliente.calcular_precio()
+        self.precio = cliente.base
 
-    def precio(self):
         if self.is_invierno():
-            return self.cliente.calcular_precio() * 0.1
-        else:
-            return self.cliente.calcular_precio()
+            self.precio *= 0.1
 
     def is_invierno(self):
         if datetime.date.today().month in [7,8]:
             return True
-        if datetime.date.today().month == 6 and datetime.date.today().day > 20:
+        elif datetime.date.today().month == 6 and datetime.date.today().day > 20:
             return True
-        if datetime.date.today().month == 9 and datetime.date.today().day < 20:
+        elif datetime.date.today().month == 9 and datetime.date.today().day < 20:
             return True
         else:
             return False
 
+
 class Cliente:
+    def __init__(self, clases):
+        self.base = 500.0
+        #self.clase = clase
+
+        for clase in clases:
+            self.base = clase.calcular_precio(self.base)
+
+
+class Turista:
     def __init__(self):
-        self.precio = 500.0
+        self.base = 500.0
+
+    def calcular_precio(self):
+        return self.base
+
+
+class Estudiante:
+    def __init__(self):
         self.descuento = 0.7
 
-    def calcular_precio(self):  # polimorfismo
-        pass
-
-class Turista(Cliente):
-    def calcular_precio(self):
-        return self.precio
+    def calcular_precio(self, base):
+        return base * self.descuento
 
 
-class TuristaEst(Cliente):
-    def calcular_precio(self):
-        return self.precio * self.descuento
+class Business:
+    def __init__(self):
+        self.recargo = 1.5
+
+    def calcular_precio(self, base):
+        return base * self.recargo
 
 
-class Business(Cliente):
-    def calcular_precio(self):
-        return self.precio * 1.5
+class Premium(Business):
+    def __init__(self):
+        super().__init__()
+        self.propina = 200.0
+
+    def calcular_precio(self, base):
+        return super().calcular_precio(base) + self.propina
 
 
-class BusinessEst(Cliente):
-    def calcular_precio(self):
-        return self.precio * 1.5 * self.descuento
-
-
-class Premium(Cliente):
-    def calcular_precio(self):
-        return self.precio * 1.5 + 200
-
-un_pasaje = Pasaje(TuristaEst())
-print(un_pasaje.precio())
+un_pasaje = Pasaje(Cliente([Estudiante(), Premium()]))
+print(un_pasaje.precio)
